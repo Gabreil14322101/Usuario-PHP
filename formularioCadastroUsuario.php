412d1ecb9e423336e0ee252cb79003dfcdf76b31
GabrielCorbetta <65062046+Gabreil14322101@users.noreply.github.com>
2020-05-12 11:05:23 -0300

    Add files via upload

---
+++ formularioCadastroUsuario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="uft-8">
+        <title></title>
+</head>
+<body>
+        <form class="" action="controlerUsuario.php" method="POST">
+        <br><label for=""> Nome: </label><br>
+            <br><input type="text" name="nome" value=""><br>
+
+        <br><label for=""> E-mail: </label><br>
+            <br><input type="email" name="email" value=""><br>
+
+        <br><label for=""> Login: </label><br>
+            <br><input type="text" name="login" value=""><br>
+
+        <br><label for=""> Senha: </label><br>
+            <br><input type="password" name="senha" value=""><br>
+
+        <br><input type="submit" name="" value="CADASTRAR"><br>
+</form>       
+</body>
+</html>
